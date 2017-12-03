f9b89ec2120fae3af0a7905eafa49739e84dfd9e
Sailesh Kushwaha <sailesh.kushwaha18@gmail.com>
2017-12-03 16:30:11 +0530

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
---insert into app_user(id,name,username,password,role) values (1,'Arun','admin','admin','ADMIN');
---insert into app_user(id,name,username,password,role) values (2,'Prince','user','user','USER');
+insert into app_user(id,name,username,password,role) values (1,'Arun','admin','admin','ADMIN');
+insert into app_user(id,name,username,password,role) values (2,'Prince','user','user','USER');
 
 
 ----insert into address(name,address,owner) values ('Mr. Harry W. Jones','Administrative Officer, Department of State, 4150 Sydney Place, Washington, DC 20521-4150',1);
@@ -7,9 +7,9 @@
 --insert into address(name,address,owner) values ('Mr. William J. Bryant','American Consul, Department of State, 6170 Peshwar Place, Washington, DC 20521-6170',2);
 
 
---INSERT INTO EXERCISE( ID , FEE , TYPE ) VALUES (1,'500','CARDIO');
---INSERT INTO EXERCISE( ID , FEE , TYPE  ) VALUES (2,'2000','GYM/CARDIO');
---INSERT INTO EXERCISE( ID , FEE , TYPE  ) VALUES (3,'1000','GYM');
+INSERT INTO EXERCISE( ID , FEE , TYPE ) VALUES (1,'500','CARDIO');
+INSERT INTO EXERCISE( ID , FEE , TYPE  ) VALUES (2,'2000','GYM/CARDIO');
+INSERT INTO EXERCISE( ID , FEE , TYPE  ) VALUES (3,'1000','GYM');
 
 --Insert into GYM_USER(id,dob,email_id,Fee_Due_Date,fee_submit_date,first_name,gender,joining_date,last_name,middle_name,phone,user_name,address_id,exercise) Values (1,'2017-11-24 05:30:00.0','test@test.com','2017-11-24 05:30:00.0','2017-11-24 05:30:00.0','SK','MALE','2017-11-24 05:30:00.0','SK','SK','1234567890','SK,SK',1,1);
 --Insert into GYM_USER(id,dob,email_id,Fee_Due_Date,fee_submit_date,first_name,gender,joining_date,last_name,middle_name,phone,user_name,address_id,exercise) Values (2,'2017-11-24 05:30:00.0','test@test.com','2017-11-24 05:30:00.0','2017-11-24 05:30:00.0','NK','MALE','2017-11-24 05:30:00.0','NK','NK','1234567890','NK,NK',2,3);
