2cd4c8d02e404218339f167b8a2495fe4086fc78
Sailesh Kushwaha <sailesh.kushwaha18@gmail.com>
2017-12-03 15:44:24 +0530

    changed the properties and users.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into app_user(id,name,username,password,role) values (1,'John','admin','admin','ADMIN');
-insert into app_user(id,name,username,password,role) values (2,'Liza','user','user','USER');
+insert into app_user(id,name,username,password,role) values (1,'Arun','admin','admin','ADMIN');
+insert into app_user(id,name,username,password,role) values (2,'Prince','user','user','USER');
 
 
 ----insert into address(name,address,owner) values ('Mr. Harry W. Jones','Administrative Officer, Department of State, 4150 Sydney Place, Washington, DC 20521-4150',1);
